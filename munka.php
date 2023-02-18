59c6938f2366511e4bfe30171160e938c556dfa0
bitobarnabas04 <bitobarnabas04@gmail.com>
2023-02-18 17:01:54 +0100

    Osztás 1

--- munka.php
+++ munka.php
@@ -26,9 +26,13 @@ print "<br /><br />" . $x - $y;
 print "Konfliktus lesz...";
 
 
+// Osztás
+//------------------
 
+$egyik = 25;
+$masik = 5;
 
-
+print ($egyik / $masik) . "<br /><br />";
 
 
 // Itt a vége. :-)
